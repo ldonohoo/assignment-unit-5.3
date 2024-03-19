d115583620fc0927beefd249e3198c43d8b0456e
Lisa Donohoo <lisa@Alisas-MacBook-Pro-2.local>
2024-03-19 13:40:22 -0500

    added search function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -13,28 +13,83 @@ function addToCollection(collection, title, artist, yearPublished) {
 }
 
 function printAlbum(album) {
-  console.log('-----------------------------------------------');
-  console.log(`New album added:`); 
-  console.log('Album Title:', album.title);
-  console.log('Album Artist:', album.artist);
-  console.log('Album Year Published:', album.yearPublished);
+  console.log(`${album.title} by ${album.artist}, published in ${album.yearPublished}`);
+}
+
+function showCollection(collection) {
+  console.log(`Printing collection:`);
+  for (album of collection) {
+    printAlbum(album);
+  }
 }
 
 let newAlbum = {};
 newAlbum = addToCollection(myCollection, 'Music to Code By', 'geekyBass', 2021);
+console.log('-----------------------------------------------');
+console.log(`New album added:`); 
 printAlbum(newAlbum);
-newAlbum = addToCollection(myCollection, 'Chedar Biscuit', 'Tall Chair Group', 1990);
+newAlbum = addToCollection(myCollection, 'Tall Chair Group', 'Cheddar Biscuit', 1990);
+console.log('-----------------------------------------------');
+console.log(`New album added:`); 
 printAlbum(newAlbum);
 newAlbum = addToCollection(myCollection, 'Falling Tray', 'hipHopHideaway', 2023);
+console.log('-----------------------------------------------');
+console.log(`New album added:`); 
 printAlbum(newAlbum);
 newAlbum = addToCollection(myCollection, 'Clouds Suck', 'Squish Mouse', 2019);
+console.log('-----------------------------------------------');
+console.log(`New album added:`); 
 printAlbum(newAlbum);
 newAlbum = addToCollection(myCollection, 'Lockdown', 'Bored Travelers', 2020);
+console.log('-----------------------------------------------');
+console.log(`New album added:`); 
 printAlbum(newAlbum);
-newAlbum = addToCollection(myCollection, 'Mellow Donuts', 'Shef', 2024);
+newAlbum = addToCollection(myCollection, 'Hula Hoopla', 'Cheddar Biscuit', 2024);
+console.log('-----------------------------------------------');
+console.log(`New album added:`); 
 printAlbum(newAlbum);
 console.log('-----------------------------------------------');
-console.log(`My collection: ${myCollection}`);
+
+showCollection(myCollection);
+
+function findByArtist(collection, findArtist) {
+  let foundAlbums = [];
+  for (let album of collection) {
+    if (album.artist === findArtist) {
+      foundAlbums.push(album);
+    }
+  }
+  return foundAlbums;
+}
+
+let findArtist = 'Cheddar Biscuit';
+let subCollection = findByArtist(myCollection, findArtist);
+console.log('-----------------------------------------------');
+console.log(`Found these albums with artist ${findArtist}`);
+showCollection(subCollection);
+
+
+function search(collection, searchParameters) {
+  let foundAlbums = [];
+  for (let album of collection) {
+    if (album.artist === searchParameters.artist &&
+        album.yearPublished === searchParameters.yearPublished) {
+          foundAlbums.push(album);
+        }
+  }
+  return foundAlbums;
+}
+
+findArtist = 'Cheddar Biscuit';
+let findYear = 2024
+let searchArguments = { artist: findArtist, yearPublished: findYear };
+subCollection = search(myCollection, searchArguments);
+console.log('-----------------------------------------------');
+console.log(`Found these albums with the criteria Artist: ${searchArguments.artist} and year published: ${searchArguments.yearPublished}`);
+showCollection(subCollection);
+
+
+
 
 // PLEASE DO NOT MODIFY THIS. Just leave it down here at the bottom. Think of it
 // as a lil' chunk of friendly code that you don't need to understand right now.
